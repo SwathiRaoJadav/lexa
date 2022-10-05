c6f3cf16e3a66c5c8d5f584ac173f1be8d25c0f6
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-10-05 18:42:24 -0400

    Update train.py

--- lexa/train.py
+++ lexa/train.py
@@ -113,12 +113,12 @@ def main(logdir, config):
     num_eval_eps = 1
     for ep_idx in range(num_eval_eps):
       ep_data_across_goals = []
+      previous_agent_state = None
       for idx in range(num_goals):
         eval_envs[0].set_goal_idx(idx)
         eval_policy = functools.partial(agent, training=False)
-        sim_out = tools.simulate(eval_policy, eval_envs, episodes=1)
-        obs, eps_data = sim_out[4], sim_out[6]
-
+        sim_out = tools.simulate(eval_policy, eval_envs, episodes=1,agent_state = previous_agent_state)
+        obs, eps_data, previous_agent_state = sim_out[4], sim_out[6], sim_out[5]
         ep_data_across_goals.append(process_eps_data(eps_data))
         video = eval_envs[0]._convert([t['image'] for t in eval_envs[0]._episode])
         executions.append(video[None])
